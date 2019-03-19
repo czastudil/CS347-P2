38881b28b23811c5276f48dc837d13edb0a997e3
Kyle Laker <kyle@laker.email>
2019-03-19 16:30:00 -0400

    Add working index.html

--- cs347project/tahours/admin.py
+++ cs347project/tahours/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Question,
+    Course,
+    Professor,
+    Student,
+    TA,
+)
+
+admin.site.register(Question)
+admin.site.register(Course)
+admin.site.register(Professor)
+admin.site.register(Student)
+admin.site.register(TA)
