981cb856c161f74774cf5d412488fb567629d4bf
Kyle Laker <kyle@laker.email>
2019-03-19 15:18:56 -0400

    Add tahours to installed apps

--- cs347project/cs347project/settings.py
+++ cs347project/cs347project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tahours.apps.TahoursConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
