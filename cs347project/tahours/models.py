f6af9d984cdf2d7fa4d2dab83a62feef5e9840a4
Cindy Zastudil <zastudcr@dukes.jmu.edu>
2019-03-19 15:16:31 -0400

    created all of the models for the database

--- cs347project/tahours/models.py
+++ cs347project/tahours/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+	number = models.CharField(max_length=10, primary_key=True)
+	name = models.CharField(max_length=50)
+
+class Student(models.Model):
+	eid = models.CharField(max_length=10, primary_key=True)
+	password = models.CharField(max_length=32)
+
+class Professor(models.Model):
+	eid = models.CharField(max_length=10, primary_key=True)
+	password = models.CharField(max_length=32)
+	courses = models.ManyToManyField(Course)
+
+class TA(models.Model):
+	eid = models.CharField(max_length=10, primary_key=True)
+	password = models.CharField(max_length=32)
+	courses = models.ManyToManyField(Course)
+
+class Question(models.Model):
+	eid = models.ForeignKey(Student, on_delete=models.CASCADE)
+	time = models.DateTimeField('time added')
+	course = models.ForeignKey(Course, on_delete=models.SET_NULL)
+	professor = models.ForeignKey(Professor, on_delete=models.SET_NULL)
+	assignment = models.CharField(max_length=20)
+	question = models.TextField(max_length=200)
+
+
+
